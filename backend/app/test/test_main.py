e89a56fba3aad1ca278accc2c9ee40f962ff3a1d
molhot <sakata19991214@gmail.com>
2026-01-21 16:35:00 +0900

    pytestが通じるようにパス、およびimportを修正

--- backend/app/test/test_main.py
+++ backend/app/test/test_main.py
@@ -1,4 +1,10 @@
-pass
+from fastapi.testclient import TestClient
+from main import app
 
-def test_main():
-    assert 6 == 6
+client = TestClient(app)
+
+def test_health_check():
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
